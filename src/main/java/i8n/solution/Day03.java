76dc8456419340bca18076cfb0bdd107441cb226
Sei-Jin <seijin.tufts@gmail.com>
2025-03-09 13:39:12 -0300

    [03] Convert solution to stream

--- src/main/java/i8n/solution/Day03.java
+++ src/main/java/i8n/solution/Day03.java
@@ -2,81 +2,33 @@ package i8n.solution;
 
 import i8n.util.InputHandler;
 
-import java.util.regex.Pattern;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
 
 public class Day03 {
     
     private static final String FILENAME = "input/day03.txt";
-    private static final Pattern NUMBER = Pattern.compile("(\\d)");
+    
+    private static final int MIN_LENGTH = 4;
+    private static final int MAX_LENGTH = 12;
+    
+    private static final Predicate<String> IS_CORRECT_LENGTH = (s ->
+        s.length() >= MIN_LENGTH && s.length() <= MAX_LENGTH
+    );
+    private static final IntPredicate IS_NOT_7_BIT_ASCII = (c -> c >= 128);
     
     public static void main(String[] args) {
         final var passwords = InputHandler.readAsLines(FILENAME);
         
-        var totalValidPasswords = 0;
+        final var valid = passwords
+            .stream()
+            .filter(IS_CORRECT_LENGTH)
+            .filter(p -> p.chars().anyMatch(Character::isDigit))
+            .filter(p -> p.chars().anyMatch(Character::isUpperCase))
+            .filter(p -> p.chars().anyMatch(Character::isLowerCase))
+            .filter(p -> p.chars().anyMatch(IS_NOT_7_BIT_ASCII))
+            .count();
         
-        for (final var password : passwords) {
-            if (isValidPassword(password)) {
-                totalValidPasswords++;
-            }
-        }
-        
-        System.out.println(totalValidPasswords);
-    }
-    
-    private static boolean isValidPassword(String password) {
-        if (password.length() < 4 || password.length() > 12) {
-            return false;
-        }
-        
-        if (!NUMBER.matcher(password).find()) {
-            return false;
-        }
-        
-        if (!containsUppercaseAlphabetic(password)) {
-            return false;
-        }
-        
-        if (!containsLowercaseAlphabetic(password)) {
-            return false;
-        }
-        
-        if (!containsOutsideAscii7Bit(password)) {
-            return false;
-        }
-        
-        return true;
-    }
-    
-    private static boolean containsUppercaseAlphabetic(String password) {
-        for (int i = 0; i < password.length(); i++) {
-            final var character = password.charAt(i);
-            if (Character.isAlphabetic(character) && Character.isUpperCase(character)) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    private static boolean containsLowercaseAlphabetic(String password) {
-        for (int i = 0; i < password.length(); i++) {
-            final var character = password.charAt(i);
-            if (Character.isAlphabetic(character) && Character.isLowerCase(character)) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    private static boolean containsOutsideAscii7Bit(String password) {
-        for (int i = 0; i < password.length(); i++) {
-            final var character = password.charAt(i);
-            if ((int) character >= 128) {
-                return true;
-            }
-        }
-        
-        return false;
+        System.out.println(valid);
     }
 }
